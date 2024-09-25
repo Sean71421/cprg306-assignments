a94334949120b91ac01e1be8ba7b5bbff9054315
Sean71421 <167254214+Sean71421@users.noreply.github.com>
2024-09-24 22:20:35 -0400

    Week 2 assignment

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,24 +1,11 @@
-import EventList from "./event-list";
 import Link from "next/link";
+import StudentInfo from "./student-info";
 
 export default function Page() {
   return (
     <main>
-      <h1>Community Event Planner</h1>
-      <EventList />
-      <div>
-        <h2>Current Weather</h2>
-        <p>Temperature: 15°C</p>
-        <p>
-          <Link href="https://www.theweathernetwork.com/ca/weather/alberta/calgary">
-            Weather forecast
-          </Link>
-        </p>
-        <p>
-          <Link href="../">Home</Link>
-          {/* The advantage over using <a> is that Link provides prefetching and client-side navigation. */}
-        </p>
-      </div>
+      <h1>Shopping list</h1>
+      <StudentInfo />
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Sean Quinn</h1>
+      <Link href="https://github.com/Sean71421/cprg306-assignments">Github</Link>
+    </main>
+  );
+}
